eeede068443f0573d8a67b918ec6bdc5c6e0df5d
Joseph Nicolas <josephnicolas10@gmail.com>
2023-03-18 17:18:50 -0400

    added button receiving inputs

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const { REST, Routes } = require('discord.js');
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle, Events } = require('discord.js');
-const { Client, GatewayIntentBits } = require('discord.js');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, Events, ComponentType } = require('discord.js');
+const { Client, GatewayIntentBits, MessageButton, MessageActionRow } = require('discord.js');
 
 require('dotenv').config();
 
@@ -22,7 +22,6 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
     console.log('Started refreshing application (/) commands.');
 
     await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
-
     console.log('Successfully reloaded application (/) commands.');
   } catch (error) {
     console.error(error);
@@ -33,41 +32,42 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.on('interactionCreate', async interaction => {
+    if (interaction.isButton()) {
+      const buttonId = interaction.customId;
+      console.log(`Button clicked: ${buttonId}`);
+      await interaction.reply(`Button clicked: ${buttonId}`);
+    }
+  });
 });
 
 client.on('interactionCreate', async interaction => {
   if (!interaction.isChatInputCommand()) return;
-
   const { commandName } = interaction;
-
   if (commandName === 'ping') {
-    await interaction.reply('Pongo!');
+    await interaction.reply('Pong!');
   }
-
   if (commandName === 'buttons') {
-
     const row = new ActionRowBuilder()
-			.addComponents(
-				new ButtonBuilder()
-					.setCustomId('Team A')
-					.setLabel('Team A')
-					.setStyle(ButtonStyle.Primary),
+      .addComponents(
         new ButtonBuilder()
-					.setCustomId('Team B')
-					.setLabel('Team B')
-					.setStyle(ButtonStyle.Primary),
+          .setCustomId('Team A')
+          .setLabel('Team A')
+          .setStyle(ButtonStyle.Primary),
         new ButtonBuilder()
-					.setCustomId('Team C')
-					.setLabel('Team C')
-					.setStyle(ButtonStyle.Primary),
-			);
+          .setCustomId('Team B')
+          .setLabel('Team B')
+          .setStyle(ButtonStyle.Primary),
+        new ButtonBuilder()
+          .setCustomId('Team C')
+          .setLabel('Team C')
+          .setStyle(ButtonStyle.Primary),
+      );
     const messageContent = "Test Message"
-   // const inputs = { components: [row1], components: [row2]}
-		await interaction.reply({ content: messageContent, components: [row]});
-   // await interaction.reply({ content: 'I think you should click button 2,', components: [row2] });
-   // await interaction.reply({ content: 'I think you should click button 3,', components: [row3] });
+    await interaction.reply({ content: messageContent, components: [row] });
   }
-
 });
+
+
 
 client.login(process.env.DISCORD_TOKEN);
