dfc6fe79170ed0dbb9e025e2f084929200c40963
Joseph Nicolas <josephnicolas10@gmail.com>
2023-03-18 17:56:42 -0400

    Separated files

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
-const { REST, Routes } = require('discord.js');
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle, Events, ComponentType } = require('discord.js');
-const { Client, GatewayIntentBits, MessageButton, MessageActionRow } = require('discord.js');
+const {REST, Routes, Client, GatewayIntentBits } = require('discord.js');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, Events, ComponentType,  MessageButton, MessageActionRow } = require('discord.js');
+const { handleButtonInteraction, handleCommandInteraction } = require('./src/interactions');
 
 require('dotenv').config();
 
-const commands = [
-  {
-    name: 'ping',
-    description: 'Replies with Pong!',
-  },
-  {
-    name: 'buttons',
-    description: 'Select one of the buttons'
-  }
-];
-
-const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+const commands = require('./src/commands');
 
 (async () => {
   try {
     console.log('Started refreshing application (/) commands.');
-
     await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
     console.log('Successfully reloaded application (/) commands.');
   } catch (error) {
@@ -34,40 +22,14 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   client.on('interactionCreate', async interaction => {
     if (interaction.isButton()) {
-      const buttonId = interaction.customId;
-      console.log(`Button clicked: ${buttonId}`);
-      await interaction.reply(`Button clicked: ${buttonId}`);
+      await handleButtonInteraction(interaction);
     }
   });
 });
 
 client.on('interactionCreate', async interaction => {
   if (!interaction.isChatInputCommand()) return;
-  const { commandName } = interaction;
-  if (commandName === 'ping') {
-    await interaction.reply('Pong!');
-  }
-  if (commandName === 'buttons') {
-    const row = new ActionRowBuilder()
-      .addComponents(
-        new ButtonBuilder()
-          .setCustomId('Team A')
-          .setLabel('Team A')
-          .setStyle(ButtonStyle.Primary),
-        new ButtonBuilder()
-          .setCustomId('Team B')
-          .setLabel('Team B')
-          .setStyle(ButtonStyle.Primary),
-        new ButtonBuilder()
-          .setCustomId('Team C')
-          .setLabel('Team C')
-          .setStyle(ButtonStyle.Primary),
-      );
-    const messageContent = "Test Message"
-    await interaction.reply({ content: messageContent, components: [row] });
-  }
+  await handleCommandInteraction(interaction);
 });
-
-
 
 client.login(process.env.DISCORD_TOKEN);
---
+++ src/commands.js
@@ -0,0 +1,11 @@
+module.exports = [
+    {
+      name: 'ping',
+      description: 'Replies with Pong!',
+    },
+    {
+      name: 'buttons',
+      description: 'Select one of the buttons'
+    }
+  ];
+  
