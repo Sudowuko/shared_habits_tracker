5236b83eb874993001106ec1d5a77295c696df33
Joseph Nicolas <josephnicolas10@gmail.com>
2023-03-25 15:58:27 -0400

    updated stats command

--- commands/stats.js
+++ commands/stats.js
@@ -3,22 +3,6 @@ const { getFirestore } = require('firebase-admin/firestore');
 
 const db = getFirestore()
 
-async function firestoreTest() {
-    const snapshot = await db.collection('users').get();
-    snapshot.forEach((doc) => {
-        console.log(doc.id, '=>', doc.data());
-    })
-}
-
-// async def viewStats(ctx, arg=None):
-//     user = ctx.author
-//     doc_ref = db.collection('users').document(str(user.id))
-//     tokens = doc_ref.get().get("tokens")
-//     monthly_logs = doc_ref.get().get("monthly_logs")
-//     team = doc_ref.get().get("team")
-//     habit = doc_ref.get().get("habit")
-//     await ctx.send(f"**Personal Stats for {ctx.author}** \n ```Tokens: {tokens} \nMonthly Logs: {monthly_logs} \nTeam: {team} \nHabit: {habit}``` ")
-
 module.exports = {
 
     data: new SlashCommandBuilder().setName('stats').setDescription('view personal stats'),
@@ -30,10 +14,11 @@ module.exports = {
         const monthlyLogs = doc.get('monthly_logs');
         const team = doc.get('team');
         const habit = doc.get('habit');
-        //     await ctx.send(f"**Personal Stats for {ctx.author}** \n ```Tokens: {tokens} \nMonthly Logs: {monthly_logs} \nTeam: {team} \nHabit: {habit}``` ")
-
-        // console.log("User: ", user)
-        // console.log("Interaction: ", interaction);
-        interaction.reply(`**Personal Stats for ${user.username}** \n Tokens: ${tokens} \nMonthly Logs: ${monthlyLogs} \nTeam: ${team} \nHabit: ${habit}`);
+        interaction.reply("```Personal Stats: " + user.username +
+            "\n Tokens: " + tokens +
+            "\nMonthly Logs: " + monthlyLogs +
+            "\nTeam: " + team +
+            "\nHabit: " + habit +
+            "\n```");
     }
 }
