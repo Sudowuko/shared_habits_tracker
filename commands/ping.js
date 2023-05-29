0a6f2676909c2fdcd2aa88fca3040a9813753a6f
Sudowuko <j2nicola@uwaterloo.ca>
2023-05-29 15:04:44 -0400

    updated basic ping

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
 	data: new SlashCommandBuilder().setName('ping').setDescription('New description pong'),
     async execute(interaction) {
-        interaction.reply("New test ping test v2")
+        interaction.reply("Test successful, ping pong!")
     }
 	
 }
