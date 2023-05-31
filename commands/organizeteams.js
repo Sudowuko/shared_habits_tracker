d0bf6d29a3efacfbbea179c0c6d45bcf86747be3
Sudowuko <j2nicola@uwaterloo.ca>
2023-05-31 16:46:25 -0400

    updated organizeteams to include randonomized sorting

--- commands/organizeteams.js
+++ commands/organizeteams.js
@@ -16,9 +16,20 @@ module.exports = {
         .setDescription('Organizes members into existing teams'),
     async execute(interaction) {
         //Get list of registered users
+        // Shuffling function
+        function shuffleArray(array) {
+            console.log("Shuffling is happening")
+            for (let i = array.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [array[i], array[j]] = [array[j], array[i]];
+            }
+            return array;
+        }
+
         let membercount = 0;
         const userRef = db.collection('users');
         const userDocs = await userRef.get();
+        const userArray = userDocs.docs.map((doc) => doc);
         const usernames = [];
 
         userDocs.forEach((userDoc) => {
@@ -26,10 +37,14 @@ module.exports = {
             const username = userDoc.data().username;
 
             if (registered === true) {
+                console.log("Before Shuffled ID: " + userDoc.id)
                 usernames.push(username);
                 membercount += 1;
             }
         });
+
+        // Shuffle the userDocs array
+        const shuffledUserDocs = shuffleArray(userArray);
 
         // Gets number of teams available
         const teamsRef = db.collection('teams');
@@ -39,23 +54,33 @@ module.exports = {
         //Get number of people needed per team
         const membersPerTeam = Math.floor(membercount / teamCount);
 
+        //Reset teams before sorting members
+        teamInfo.forEach((teamDoc) => {
+            const teamRef = teamsRef.doc(teamDoc.id);
+
+            teamRef.update({
+                member_list: [] // Clear the member_list by assigning an empty array
+            });
+        });
+
+
         //Sort members into teams collection
         let currentTeamIndex = 0;
         let currentMembers = 0;
-
-        userDocs.forEach((userDoc) => {
+        shuffledUserDocs.forEach((userDoc) => {
             const userID = userDoc.id;
             const registered = userDoc.data().registered;
 
             if (registered === true) {
+                console.log("After Shuffled ID: " + userDoc.id)
                 const teamDocs = teamInfo.docs;
                 if (currentTeamIndex < teamDocs.length) {
                     const teamRef = teamsRef.doc(teamDocs[currentTeamIndex].id);
                     const teamName = teamDocs[currentTeamIndex].data().team_name;
-                    console.log("User ID: " + userID)
-                    console.log("Team Name: " + teamName)
-                    console.log("Team index: " + currentTeamIndex)
-                    console.log("Current Member Count: " + currentMembers)
+                    // console.log("User ID: " + userID);
+                    // console.log("Team Name: " + teamName);
+                    // console.log("Team index: " + currentTeamIndex);
+                    // console.log("Current Member Count: " + currentMembers);
 
                     teamRef.update({
                         member_list: admin.firestore.FieldValue.arrayUnion(userID),
@@ -74,8 +99,6 @@ module.exports = {
         });
 
 
-
-
         //Using the teams collection, print out all the team names along with each member associated to it
 
         let outputMessage = '';
@@ -87,7 +110,8 @@ module.exports = {
 
             outputMessage += `Team: ${teamName}\n`;
 
-            for (const member of members) {
+            for (let i = 0; i < members.length; i++) {
+                const member = members[i];
                 const userRef = db.collection('users').doc(member);
                 const userSnapshot = await userRef.get();
                 const userData = userSnapshot.data();
@@ -95,6 +119,7 @@ module.exports = {
 
                 outputMessage += `- ${username}\n`;
             }
+
             outputMessage += '\n';
         }
 
