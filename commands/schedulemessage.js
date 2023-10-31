e424e6081a3cf383abe5c8ec6bb6d9955c3e59df
Sudowuko <j2nicola@uwaterloo.ca>
2023-10-31 17:08:22 -0400

    WIP

--- commands/schedulemessage.js
+++ commands/schedulemessage.js
@@ -4,13 +4,24 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('schedulemessage')
         .setDescription('Schedules a daily message for the competition'),
-    execute: async (interaction) => {
-        const { commands } = interaction.client;
-        
-        // Your daily message logic here
-        const dailymessageCommand = commands.get('dailymessage');
-        if (dailymessageCommand) {
-            dailymessageCommand.execute(interaction);
-        }
+    async execute(interaction) {
+        // Acknowledge the command
+        // interaction.reply('Scheduling the daily message...');
+
+        // Define the delay in milliseconds (10 seconds)
+        const delay = 2 * 1000;
+
+        // Use setTimeout to execute the dailymessage logic after the specified delay
+        setTimeout(() => {
+            console.log("Test 0");
+            // Your daily message logic here (call dailymessage or include its logic here)
+            const dailymessageCommand = interaction.client.commands.get('dailymessage');
+            console.log("Test 1")
+            if (dailymessageCommand) {
+                console.log("Test 2")
+                dailymessageCommand.execute(interaction);
+                console.log("Test 3")
+            }
+        }, delay);
     },
 };
