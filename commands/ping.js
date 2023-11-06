b49aedc716d39004df4b0c0d5191b7b648e4e80e
Sudowuko <j2nicola@uwaterloo.ca>
2023-11-06 15:16:08 -0500

    using ping as a test command. Can currently send repeated messages on a specific time interval

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,26 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
+// An array to store the interval IDs
+const intervals = {};
+
 module.exports = {
-	data: new SlashCommandBuilder().setName('ping').setDescription('New description pong'),
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Send repeated messages every 10 seconds'),
+
     async execute(interaction) {
-        interaction.reply("Test successful, ping pong!")
-    }
-	
-}
+        const userId = interaction.user.id;
+
+        // Check if there's already an interval running for this user
+        if (intervals[userId]) {
+            interaction.reply('You already started the ping interval.');
+        } else {
+            // Start a new interval for the user
+            intervals[userId] = setInterval(() => {
+                interaction.followUp('Ping!');
+            }, 10000); // 10000 milliseconds = 10 seconds
+
+            interaction.reply('Ping interval started! You will receive a message every 10 seconds.');
+        }
+    },
+};
